628a3af0194409144e71ccaf5cdbcd228158f70c
harikan23 <36985848+harikan23@users.noreply.github.com>
2023-05-06 15:43:25 -0700

    API URL

--- dashboard_microservice/dashboard/urls.py
+++ dashboard_microservice/dashboard/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
+
 
 urlpatterns = [
-    path('', views.sign_in_page, name='sign_in'),
-    path('users', views.user_details, name='user Profile'),
-    path('videoCategoryCount', views.video_category_wiseCount, name='video Category Count'),
-    path('videoCategoryList', views.video_category_like, name='video Category likes'),
-    path('videoCategoryDislike', views.video_category_dislike, name='video Category Dislike'),
+    # path('login/', auth_views.LoginView.as_view(template_name='pages/login.html'), name='login'),
+    path('login/', views.sign_in_page, name='login'),
+    path('logout', views.sign_out_page, name='logout'),
+    path('users', views.user_details, name='user-Profile'),
+    path('videoCategoryCount', views.video_category_wiseCount, name='video-Category-Count'),
+    path('videoCategoryList', views.video_category_like_dislike, name='video-Category-likes-dislike'),
+    path('videoCount', views.video_count, name='video-count'),
+    path('userCount', views.total_users, name='users-count'),
 ]
